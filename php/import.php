e9e57628126b731fc7e2940a43152e2406093bc5
DESKTOP-U4K4SH7\Faurever <jeromefaure93@orange.fr>
2018-03-22 20:55:26 +0100

    V1: Fin du proto Solve #1

--- php/import.php
+++ php/import.php
@@ -1,4 +1,5 @@
 <?php
+echo("Test de rentré héhé");
 extract(filter_input_array(INPUT_POST) );
 $fichier=$_FILES["userfile"]["name"];
 if ($fichier){
@@ -18,6 +19,7 @@ if ($fichier){
 
 <?php
 echo("Importation faite !");// importation
+echo "<br/>\n<br/><br/><br/>";
 //declaration de la variable cpt qui permettra de compter le nombre d'enregistrement réalisé
 //C:\wamp64\www\import.php
 $cpt=0;
@@ -33,13 +35,13 @@ while(!feof($fp)){
 	$liste[1] = ( isset($liste[1]) ) ? $liste[1] : Null;
 	$liste[2] = ( isset($liste[2]) ) ? $liste[2] : Null;
 	$liste[3] = ( isset($liste[3]) ) ? $liste[3] : Null;
-	echo "liste[0]=>";
+	echo "Premier element trouvé est (liste[0])=>";
 	echo $liste[0];
 	echo " !";
-	$champs1= $liste[0].'"';
-	$champs2= $liste[1];
-	$champs3= $liste[2];
-	$champs4= '"'.$liste[3];
+	$champs1= $liste[0].'"'; //Correspond à la requete écrite dans le fic.csvcsv
+	$champs2= $liste[1];	//Element 2 du fic.csv
+	$champs3= $liste[2];	//Element 3 du fic.csv
+	$champs4= '"'.$liste[3];	//Element 4 du fic.csv
 	echo "<br/>\n<br/><br/><br/>";
 	echo "champs1=>";
 	echo $champs1."<br/>";
@@ -48,45 +50,54 @@ while(!feof($fp)){
 	echo " !";
 	echo "<br/>\n<br/><br/><br/>";
 	$db = null;
-	$datetest = "2017-10-11";
+	//En cas d'intégration à la BDD, variable prenant la date d'aujourdui
+	//$datetest = $champs4;
+	$datetest = "2015-10-22";
 	$db = new mysqli('mysql-linkyu.alwaysdata.net', 'linkyu_madera', 'M@d3r4', 'linkyu_madera_db_test');
 		if ($db->connect_error) {
-    	die("Connection failed: " . $db->connect_error);
-}
-		$sql2 = "SELECT Requete FROM log WHERE DateReq = '2017-10-11'"; 
+			die("Connection failed: " . $db->connect_error);
+		}
+		$sql2 = "SELECT Requete FROM log WHERE DateReq = '$datetest'"; 
 		$result2 = $db-> query($sql2)->fetch_assoc();
-		echo "Result2 partiel taille ->";
+		echo "Result2 de la requete SELECT:  taille -> ";
 		$size =  sizeof($result2);
 		echo $size;
+		echo "<br/>\n<br/>";
 		if($size == 0){
+			//Alors pas de resultat à cette date dans la BDD
 			$result2 = '';
+			echo "Pas de résultat pour cette date dans la BDD (doit etre vide)!";
 		}else{
+			//On a un résultat à cette date dans la BDD donc il n'y aura pas d'intégration à faire
 			$result2 = current($result2);
-			echo "Result2 else ->";
-			echo $result2;
+			echo "Occurence trouvée pour cette date dans la BDD (!=vide) ->";
+			//echo $result2;
 		}
 		
 		
 		//Prévoir boucle car il y aura plusieurs élément !!
 		//$result = $result2[0]
 		//$result2 = $result2['Requete'];
-		echo "<br/>\n<br/><br/><br/>";
+		echo "<br/>\n<br/>";
 		echo "Result2  ->";
 		echo $result2;
-		echo " !!";	
+		echo " !!";
+		echo "<br/>\n<br/><br/><br/>";		
 
-
-	if ($champs1==''){
-		echo "champs vide, pas de requete ->";
-		exit;
+		//Vérif si ya une requete à intégrer dans la BDD venant du fic.csv 
+	if ($champs1=='"'){
+		echo "champs vide, pas de requete, sauter cette ligne";
+		//exit;
+		break;
 	}
-	if ($result2 ==''){
+	else if ($result2 ==''){
+		//Comme on a pas de résultat dans la BDD alors on tente d'intégrer 
 		if ($result2  == $champs1){
-
+			
 			//COMPARER DATE !!!!!!!!!!!!!!!!!!!
-
-			echo "Champs identique";
-			//A ne pas inserer dans la database
+			
+			echo "Champs identique"."<br/>";
+			//A ne pas inserer dans la database car la requete déjà écrite dans la BDD est identique à celle qu'on veut intégrer
 			echo"Pas d'integration dans les log";
 
 		}
@@ -95,31 +106,34 @@ while(!feof($fp)){
 
 				//Ajout de la requete à la table log
 				$champs1 = substr($champs1, 1, -1);
-				echo "Test chaps1: ".$champs1." et date=".$datetest;
+				$sql2 = $champs1;
+				echo "On va tenter d'inserer dans la table log -> ".$champs1." et avec pour date= ".$datetest;
 				$champs1 = addslashes($champs1);
-				$sql = "INSERT INTO log (Requete, DateReq) VALUES ('$champs1', '$datetest')";
+				echo "<br/>\n<br/>";
+				//$sql = "INSERT INTO log (Requete, DateReq) VALUES ('$champs1', '$datetest')";
+				$sql = "INSERT INTO log (Requete, DateReq) VALUES ('$champs1', '$champs4')";
 				$result = $db-> query($sql);
-				echo "Result -> ".$result;
+				echo "<br/>\n<br/>";
+				echo "Result de l'insert dans la BDD -> ".$result;
 				echo "<br/>\n<br/><br/><br/>";
 				//echo "Champs non identique";
-				echo "Result est vide, du coup jy rentre les valeurs, champs1  ->";
-				echo $champs1;
-				echo " !!";		
+				//echo "Result est vide, du coup jy rentre les valeurs, champs1  ->".$champs1;
+				//echo " !!";		
 
 
 				//Execution de la requete
-				$sql = $champs1;
-				$sql = substr($sql, 1);
-				echo "<br/>\n<br/><br/><br/>";
-				echo "Valeur de sql  ->";
-				echo $sql;
+				echo "New Requete sql qu'on va executer dans la BDD ->";
+				echo $sql2;
 				echo " !!";
-					$result3 = $db-> query($sql);
+				$result3 = $db-> query($sql2);
 
 				echo "<br/>\n<br/><br/><br/>";
-				echo "Result3  ->";
-				echo $result3;
+				echo "Resultat de la tentative de l'execution  ->".$result3;
+				//echo $result3;
 				echo " !!";
+				
+				echo "<br/>\n<br/>FIN DE TRAITEMENT POUR CETTE LIGNE<br/><br/><br/><br/>";
+				$cpt++;
 			}
 	}
 	
@@ -153,7 +167,7 @@ while(!feof($fp)){
 	//echo ""."\n";
 
 	//echo "Ligne Suivante"."\r\n".$test;
-	echo "<br/>\n<br/><br/><br/>";
+	echo "Fin<br/>\n<br/><br/><br/>";
 }
 //fermeture du fichier
 fclose($fp);
